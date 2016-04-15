7234e13e22ee1a717bc6d8ffb86877c7e3e03a7b
Boris Mikhaylov <kaguxmail@gmail.com>
2016-04-15 12:24:59 +0200

    extracted cli parsing

--- main.go
+++ main.go
@@ -4,34 +4,17 @@ import (
 	"fmt"
 	"github.com/kagux/go-remote-cli/client"
 	"github.com/kagux/go-remote-cli/server"
-	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
 )
 
 func main() {
-	isServer := kingpin.Flag("server", "Run in server mode").Short('s').Bool()
-	cmd := kingpin.Flag("cmd", "[Client mode] Command to run").Short('c').String()
-	host := kingpin.Flag("host", "Host to bind to or to connect to").Short('h').Default("0.0.0.0").String()
-	port := kingpin.Flag("port", "Port to bind to or to connect to").Short('p').Default("9201").Int()
-	kingpin.Version("0.0.1")
-	kingpin.Parse()
-
+	opts := ParseCLI()
 	var err error
-	if *isServer {
-		s := server.New(&server.Options{
-			Host: *host,
-			Port: *port,
-		})
-		err = s.Run()
+	if opts.IsServer {
+		 err = server.New(opts.ServerOptions).Run()
 	} else {
-		c := client.New(&client.Options{
-			Cmd:  *cmd,
-			Host: *host,
-			Port: *port,
-		})
-		err = c.Run()
+		err = client.New(opts.ClientOptions).Run()
 	}
-
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
